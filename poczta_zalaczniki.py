40a355908abaed15c6b080533f260483924b8ae7
ArkadioG <frank.gdansk@gmail.com>
2017-03-16 23:55:51 +0100

    zadanie i usuniecie maila
    usuniecie pliku z mailem

---
+++ poczta_zalaczniki.py
@@ -0,0 +1,50 @@
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+class Poczta(object):
+
+    def __init__(self, login, haslo, server='smtp.gmail.com'):
+        """Tworzy serwer pocztowy"""
+        self.login = login
+        self.haslo = haslo
+        self.server = server
+
+    def send_mail(self, send_to, subject, text, files=None):
+        """
+        Wysyła wiadomość email do użytkownika w formacie utf-8
+        :param send_to: Lista odbiorców [str]
+        :param subject: Temat wiadomości
+        :param text: Tresc wiadomości
+        :param files: lista plików do przesłania [str]
+        :return: None
+        """
+        assert isinstance(send_to, list)
+
+        msg = MIMEMultipart()
+        msg['From'] = self.login
+        msg['To'] = COMMASPACE.join(send_to)
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = subject
+
+        msg.attach(MIMEText(text, _charset='utf-8'))
+
+        for f in files or []:
+            with open(f, "rb") as fil:
+                part = MIMEApplication(
+                    fil.read(),
+                    Name=basename(f)
+                )
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+                msg.attach(part)
+
+
+        mailer = smtplib.SMTP(self.server, 587)
+        mailer.ehlo()
+        mailer.starttls()
+        mailer.login(self.login, self.haslo)
+        mailer.sendmail(self.login, send_to, msg.as_string())
+        mailer.close()
---
+++ wyslij_zalacznik.py
@@ -0,0 +1,15 @@
+import poczta_zalaczniki
+import os
+import secrets
+
+# odczytujemy argumenty: nazwa, rozmiar
+# w danym katalogu zmieniamy zdjęcia, zapisujemy je pod nową nazwą
+# tworzymy listę z pełnymi ścieżkami plików
+# -------------------------------
+# wysłanie wiadomości
+# tworzymy serwer (login, hasło, server)
+# wysyłamy wiadomość (lista odbiorcow, temat, tresc, lista zalacznikow)
+
+
+# podpowiedz: mozna uzyc moduł do zmiany plików, tylko trzeba go lekko
+# przerobić
