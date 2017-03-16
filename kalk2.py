517b488ae8dac5e1e5b7ffec4bedfbb924d9ff25
ArkadioG <frank.gdansk@gmail.com>
2017-03-16 19:26:14 +0100

    argumenty z konsoli

---
+++ kalk2.py
@@ -0,0 +1,16 @@
+import sys
+
+# print(sys.argv)
+def divide(a, b):
+    return a / b
+
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    wynik = divide(a, b)
+    print(wynik)
+
+if __name__ == '__main__':
+    main()
